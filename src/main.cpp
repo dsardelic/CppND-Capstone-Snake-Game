c690cfa9adbf973b49452cee6ad16ab372ec7d11
Dino Sardelić <25157560+dsardelic@users.noreply.github.com>
2024-01-30 02:09:39 +0100

    Clear screen upon game launch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include <stdlib.h>  // system
+
 #include <iostream>  // std::cout
 
 #include "menu.h"  // Menu
 
 int main() {
+  system("clear");
   std::cout << "WELCOME TO SNAKE GAME!" << std::endl;
   std::cout << std::endl;
   Menu menu;
